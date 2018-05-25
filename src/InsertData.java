be6a86da54351f67502a05c0b41dbcbf797111bc
kanchan5495 <kanchanshinde5495@gmail.com>
2018-05-25 14:05:01 +0530

    insertdata file modifed

--- src/InsertData.java
+++ src/InsertData.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class InsertData
+ * New some 
  */
 
 //Author kanchan
